f3378679ec95d44f1aa674e83126b44aa56ec539
Amalia Isabel Rosales Vicente <arosalesv9@miumg.edu.gt>
2026-03-21 23:11:37 -0600

    Array bidimensional

---
+++ ARRAY BIDIMENSIONAL/ARRAY BIDIMENSIONAL - MATRICES.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+
+main(){
+	int fil=0, col=0;
+	cout<<"Ingrese Filas: "	;
+	cin>>fil;
+	cout<<"Ingrese Columnas: ";
+	cin>>col;
+	int matriz[fil][col];
+
+cout<<"------------Ingresar----------"<<endl;
+	
+	for(int i=0;i<fil;i++){
+		for(int ii=0;ii<col;ii++){
+			cout<<i<<","<<ii<<": ";
+			cin>>matriz[i][ii];
+			
+		}
+		cout<<"______________"<<endl;
+	}
+
+
+
+	cout<<"------------Mostrar----------"<<endl;
+	
+	for(int i=0;i<fil;i++){
+		for(int ii=0;ii<col;ii++){
+			cout<<i<<","<<ii<<": "<<matriz[i][ii]<<endl;
+			
+		}
+		
+	}
+	
+
+	
+	
+}
---
+++ ARRAY BIDIMENSIONAL/ARRAY BIDIMENSIONAL - Simple.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+main(){
+	
+	int matriz[2][3]={{10,20,30},{40,50,60}};
+	
+	for(int i=0)
+	
+	
+	cout<<"0,0: "<<matriz[0][0]<<endl;
+	cout<<"0,1: "<<matriz[0][1]<<endl;
+	cout<<"0,2: "<<matriz[0][2]<<endl;
+	cout<<"1,0: "<<matriz[1][0]<<endl;
+	cout<<"1,1: "<<matriz[1][1]<<endl;
+	cout<<"1,2: "<<matriz[1][2]<<endl;
+	
+	system("pause");
+	
+}
